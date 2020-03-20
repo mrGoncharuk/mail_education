fa7bb591a9da526a992ec96a5a3504c6204049e0
Oleksandr Iegorov <alex.yegorov@gmail.com>
2020-03-20 14:29:28 +0200

    Small workaround for Remi repo

--- manifests/install_php3.pp
+++ manifests/install_php3.pp
@@ -1,24 +1,29 @@
 
-$package_php = ['php-ldap', 'php-imagick', 'php-common', 'php-gd', 'php-imap', 'php-json', 'php-curl', 'php-zip', 'php-xml', 'php-mbstring', 'php-bz2', 'php-intl', 'php-gmp']
+$package_php = ['php-ldap', 'php-imagick', 'php-mysql','php-common', 'php-gd', 'php-imap', 'php-json', 'php-curl', 'php-zip', 'php-xml', 'php-mbstring', 'php-bz2', 'php-intl', 'php-gmp']
 
 
-package { "https://rpms.remirepo.net/enterprise/remi-release-8.rpm":
-    ensure   => 'present',
+#package { "Remi Repo":
+#    provider => 'rpm',
+#    ensure   => installed,
+#    source => 'https://rpms.remirepo.net/enterprise/remi-release-8.rpm',
+#}
+
+exec { 'Remi Repo':
+    command => '/usr/bin/dnf -y install https://rpms.remirepo.net/enterprise/remi-release-8.rpm',
 }
 
 exec { 'module reset php':
     command  => '/usr/bin/dnf -y module reset php',
+    require  => Exec['Remi Repo'],
 }
 
-exec { 'module enable eset php':
+exec { 'module enable php':
     command  => '/usr/bin/dnf -y module enable php:remi-7.4',
+    require  => Exec['module reset php'],
 }
 
 
 package { $package_php:
     ensure   => 'installed',
-}
-
-package { "php-mysql":
-   ensure => 'installed',
+    require => Exec['module enable php'],
 }
