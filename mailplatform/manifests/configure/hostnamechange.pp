1e3dc1a2cb22e183dcd2471bf2209b3f12103179
Alexander Yegorov <ayegorov@hostopia.com>
2020-04-14 10:09:12 -0400

    Added idempotancy for hostname setup. Fact is checked.

--- mailplatform/manifests/configure/hostnamechange.pp
+++ mailplatform/manifests/configure/hostnamechange.pp
@@ -6,10 +6,10 @@
 #   include mailplatform::configure::hostnamechange
 class mailplatform::configure::hostnamechange {
   $new_hostname = 'allinone-mh.localhost'   ## TODO: add to facts
-
-  exec { 'change hostname':
-    command  => "/usr/bin/hostnamectl set-hostname ${new_hostname}",
-    onlyif   => "if [['hostname' == ${new_hostname}]]; then exit  1; else exit 0; fi;",
-    provider => 'shell',
+  if $facts['fqdn'] != $new_hostname {
+    exec { 'change hostname':
+      command  => "/usr/bin/hostnamectl set-hostname ${new_hostname}",
+      provider => 'shell',
+    }
   }
 }
