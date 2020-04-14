44f8cb77e3dbf14f2ec163e3db786afed36aa261
Alexander Yegorov <ayegorov@hostopia.com>
2020-04-14 13:20:16 -0400

    Reworked repo installation a bit. reset/enable/disable dnf module functionality is under development at https://tickets.puppetlabs.com/browse/PUP-10235

--- mailplatform/manifests/installpackages/rc_php.pp
+++ mailplatform/manifests/installpackages/rc_php.pp
@@ -32,13 +32,17 @@ class mailplatform::installpackages::rc_php {
   }
 
   # Php installing
-  exec { 'Remi Repo':
-    command => '/usr/bin/dnf -y install https://rpms.remirepo.net/enterprise/remi-release-8.rpm',
+  package { 'Remi Repo':
+    ensure => present,
+    source => 'https://rpms.remirepo.net/enterprise/remi-release-8.rpm',
   }
+#  exec { 'Remi Repo':
+#    command => '/usr/bin/dnf -y install https://rpms.remirepo.net/enterprise/remi-release-8.rpm',
+#  }
 
   exec { 'module reset php':
     command => '/usr/bin/dnf -y module reset php',
-    require => Exec['Remi Repo'],
+    require => Package['Remi Repo'],
   }
 
   exec { 'module enable php':
